c9222784cf23099ef13c9c6f524178d5989f7811
LilleJohs <33736881+LilleJohs@users.noreply.github.com>
2017-12-13 01:14:47 -0800

    Update raiRichList.py

--- raiRichList.py
+++ raiRichList.py
@@ -1,7 +1,7 @@
 import requests
 from raiMain import *
 
-payload = {'limit': '100'}
+payload = {'limit': '2000'}
 r = requests.get('https://raiblocks.net/page/frontiers.php', payload)
 #print(r.text)
 text = r.text
@@ -12,12 +12,10 @@ for i in range(1, len(stringList)):
 	print(stringList[i][0:64])
 	raiAcc.append(stringList[i][0:64])
 	
-sum = 0
 percentSum = 0
 totalSupply = 133248290
 
 for i in range(0, len(raiAcc)):
 	thisBalance = mrai_account_balance(raiAcc[i], "pending")
-	sum = sum + thisBalance
 	percentSum = percentSum + thisBalance/totalSupply
-	print(sum, percentSum)
+	print("Richlist number:",i+1, "  MXRB in account:", thisBalance, "  Accumulated percentage:", percentSum)
